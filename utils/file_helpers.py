59ed67f9f89e1f801845d78723d1690dca01f20b
Aditya Satrya <aditya.satrya@nuesto.net>
2018-12-02 23:41:38 +0700

    add file util and requirements

--- utils/file_helpers.py
+++ utils/file_helpers.py
@@ -12,3 +12,6 @@ def remove_trailing_slash(str):
 
 def join_path_segment(separator, segment1, segment2):
 	return remove_trailing_slash(segment1) + separator + remove_leading_slash(segment2)
+
+def get_filename_from_path(path):
+	return path[path.rfind('/'):]
